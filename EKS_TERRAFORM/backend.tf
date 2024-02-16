fd6f67fb89801a1b8eec6460d8cd216c2aa26912
Alfiya-git <137032154+Alfiya-git@users.noreply.github.com>
2024-02-16 15:50:34 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend" # Replace with your actual S3 bucket name
+    bucket = "backend-alfiya" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
